54e468903865bcd25bd09a3696b6a5c9a63ed6c6
Yaroslav Kravtsov <yaroslav.kravtsov@debricked.com>
2023-11-14 21:40:12 +0100

    feat(#1): fix bandit ci check

--- features/steps/tutorial.py
+++ features/steps/tutorial.py
@@ -6,8 +6,8 @@ def step_impl(context):
 
 @when('we implement a test')
 def step_impl(context):
-    assert True is not False
+    assert True is not False, "true is not false"
 
 @then('behave will test it for us!')
 def step_impl(context):
-    assert context.failed is False
+    assert context.failed is False, "context.failed is false"
